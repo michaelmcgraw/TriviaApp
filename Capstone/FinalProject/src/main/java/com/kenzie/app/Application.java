424d742c03f7eb70359b68500d19b1ceacc4d84a
michaelmcgraw <michael.mcgraw@mykenzie.snhu.edu>
2023-01-09 19:48:53 -0600

    Finished Capstone Project

--- Capstone/FinalProject/src/main/java/com/kenzie/app/Application.java
+++ Capstone/FinalProject/src/main/java/com/kenzie/app/Application.java
@@ -21,6 +21,7 @@ public class Application {
         //Write main execution code here
         int questions = 0;
         int points =0;
+        //loops until 10 questions are asked
         while (questions < 10) {
             try{
                 //Generate random number as ID for question
@@ -28,19 +29,21 @@ public class Application {
                 int randomNumber = rand.nextInt(100) + 1;
                 //Make get request to get random clue
                 String randomClue = CustomHttpClient.sendGET("https://jservice.kenzie.academy/api/clues/" + randomNumber);
-
+                //Convert from JSON to DTO
                 ObjectMapper objectMapper = new ObjectMapper();
                 Clues clue = objectMapper.readValue(randomClue, Clues.class);
-
+                //Print Category/Question
                 System.out.println("Category: "+clue.getCategory().getTitle()+" Question: "+clue.getQuestion());
                 Scanner scan = new Scanner(System.in);
+                //User enters answer
                 System.out.println("Enter your answer: ");
                 String answer = scan.nextLine();
                 checkForEmptyString(answer);
 
+                //Replaces white space in string
                 String correctAnswer = clue.getAnswer().replaceAll("\\s","");
                 answer= answer.replaceAll("\\s","");
-
+                //checks if user entered correct answer
                 if (answer.equalsIgnoreCase(correctAnswer)){
                     System.out.println("Correct Answer!");
                     points++;
@@ -55,6 +58,7 @@ public class Application {
             }
             questions++;
         }
+        //prints out total points
         System.out.println("Your total points are: "+points);
     }
     public static void checkForEmptyString(String input){
